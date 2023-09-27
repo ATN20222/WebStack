ee4c2d7628d2482f4affd701098905ecfa4d8aee
ATN20222 <antonabdalla30@gmail.com>
2023-09-28 01:20:04 +0300

    EditCarsuel

--- main.js
+++ main.js
@@ -32,8 +32,12 @@ $(document).ready(function(){
     $(".owl-carousel").owlCarousel({
         loop:true,
     margin:10,
-    nav:false,
+    nav:true,
     autoplay:false,
+    navText: [
+        "<i class='fa fa-angle-left'></i>",
+        "<i class='fa fa-angle-right'></i>"
+    ],
     autoplayTimeout:3000,
     autoplayHoverPause:true,
     center: true,
